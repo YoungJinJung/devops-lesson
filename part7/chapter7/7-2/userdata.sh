a4e6f909a056189a4357ae15e3c15f478559d870
kuma.jung <yj.jung5078@gmail.com>
2023-03-10 16:44:51 +0900

    modify

--- part7/chapter7/7-2/userdata.sh
+++ part7/chapter7/7-2/userdata.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 sudo yum update -y
-sudo yum install -y java-17-amazon-corretto git dockerd
+sudo yum install -y java-17-amazon-corretto git docker
 sudo systemctl start docker
 
-curl -SL https://github.com/docker/compose/releases/download/v2.16.0/docker-compose-linux-x86_64 -o /usr/local/bin/docker-compose chmod 755 /usr/local/bin/docker-compose
+curl -SL https://github.com/docker/compose/releases/download/v2.16.0/docker-compose-linux-x86_64 -o /usr/local/bin/docker-compose 
+chmod 755 /usr/local/bin/docker-compose
 ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
 
 DD_API_KEY=<DATADOG_API_KEY> DD_SITE="datadoghq.com" bash -c "$(curl -L https://s3.amazonaws.com/dd-agent/scripts/install_script_agent7.sh)"
